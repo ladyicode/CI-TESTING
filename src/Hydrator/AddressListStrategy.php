3de215829b3a9585cb4648da6c05830cc38ce7e6
ladyicode <92449416+ladyicode@users.noreply.github.com>
2020-11-04 16:09:26 +0000

    clean code.

--- src/Hydrator/AddressListStrategy.php
+++ src/Hydrator/AddressListStrategy.php
@@ -4,4 +4,10 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New 
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\Hydrator;
+
+use Sake\BlockchainWalletApi\Exception\RuntimeException;
+use Zend\Stdlib\Hydrator\Strategy\StrategyI
