12443d335b92908f8c04ea203d1e8ae253899c45
ladyicode <92449416+ladyicode@users.noreply.github.com>
2020-11-04 12:35:06 +0000

    Update js.js

--- src/Hydrator/AddressListStrategy.php
+++ src/Hydrator/AddressListStrategy.php
@@ -2,4 +2,6 @@
 /**
  * Sake
  *
- * @link      http://github.com/sandrokeil/BlockchainWalletA
+ * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New 
