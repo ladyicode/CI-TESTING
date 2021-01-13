bc30619c47ad26df85fff2f0c937e3cef6d96eb7
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-01-13 20:58:37 +0000

    reformatting

--- src/Request/RequestInterface.php
+++ src/Request/RequestInterface.php
@@ -4,4 +4,12 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokei
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\Request;
+
+/**
+ * Request interface
+ *
+ * Defines necessary methods to 
