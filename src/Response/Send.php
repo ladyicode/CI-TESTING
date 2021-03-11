fe28bf2073cea7655f6b60cd3154766cf2e940f5
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-03-11 13:00:57 +0000

    Added Latvian translation.

--- src/Response/Send.php
+++ src/Response/Send.php
@@ -4,4 +4,12 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/BlockchainWal
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\Response;
+
+/**
+ * Response for payment request
+ *
+ * This class co
