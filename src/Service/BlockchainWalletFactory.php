55b04fa3ba05023b04319f414b4d86848ea9d13b
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-03-28 22:18:19 +0000

    config refactor

--- src/Service/BlockchainWalletFactory.php
+++ src/Service/BlockchainWalletFactory.php
@@ -4,4 +4,9 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license  
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\Service;
+
+use Sake
