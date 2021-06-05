32d43e56de585f8e5009fcdea874ea37c2571890
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-06-05 06:07:27 +0000

    Update release-notes.md

--- src/Service/InputFilter/AddressUnarchiveFactory.php
+++ src/Service/InputFilter/AddressUnarchiveFactory.php
@@ -3,4 +3,7 @@
  * Sake
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
- * @copyright Copyright (c) 201
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
