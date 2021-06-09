e30cb970fb94450452b23aa8a6e13c8359729365
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-06-09 06:26:13 +0000

    - Update README.md

--- src/Service/InputFilter/WalletBalanceFactory.php
+++ src/Service/InputFilter/WalletBalanceFactory.php
@@ -4,4 +4,10 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/Blockchai
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\Service\InputFilter;
+
+use Zend\InputFilter\InputFilter;
+use Zend\ServiceManager\FactoryI
