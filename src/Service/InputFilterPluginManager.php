e340d1f77f60489c42daeab39ec50474df918657
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-06-12 06:51:31 +0000

    New generation.

--- src/Service/InputFilterPluginManager.php
+++ src/Service/InputFilterPluginManager.php
@@ -4,4 +4,7 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @lice
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\Blockchai
