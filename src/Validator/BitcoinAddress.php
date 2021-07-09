b6aee73f0d18382563c34f7f90f1a9fc568296a7
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-07-09 19:49:43 +0000

    .......

--- src/Validator/BitcoinAddress.php
+++ src/Validator/BitcoinAddress.php
@@ -4,4 +4,9 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LI
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\Validator;
+
+use Sake\BlockchainWalletApi\Validator\Exception\ValidatorExcept
