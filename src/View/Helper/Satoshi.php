8d73338028a6ba2296b7b355b7f94d20da70ee7e
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-08-23 06:41:11 +0000

    fixed margins

--- src/View/Helper/Satoshi.php
+++ src/View/Helper/Satoshi.php
@@ -4,4 +4,12 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/BlockchainWalletA
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace Sake\BlockchainWalletApi\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * S
