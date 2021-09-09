bec71fedab9d975f38849b6f21f73404401c4efc
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-09-09 19:36:05 +0000

    Version 1.0.2

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -4,3 +4,10 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace SakeTest\BlockchainWalletApi;
+
+// set error reporting
+error_repo
