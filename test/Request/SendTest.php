02b7192ecf573fd3bcf0993c8e5de8c5f7634e7c
ladyicode <92449416+ladyicode@users.noreply.github.com>
2021-11-18 18:47:26 +0000

    Updating ChangeLog.md

--- test/Request/SendTest.php
+++ test/Request/SendTest.php
@@ -4,4 +4,10 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LI
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace SakeTest\BlockchainWalletApi\Request;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Sake\BlockchainWallet
