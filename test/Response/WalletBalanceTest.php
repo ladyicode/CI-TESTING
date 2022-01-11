11049877b635fe2e2b93828c1448fdfb54d1b70d
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-01-11 17:06:52 +0000

    Work on multiple lexers: t+

--- test/Response/WalletBalanceTest.php
+++ test/Response/WalletBalanceTest.php
@@ -2,4 +2,7 @@
 /**
  * Sake
  *
- * @link      http://github.com/sandrokeil/BlockchainWalletApi for th
+ * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ 
