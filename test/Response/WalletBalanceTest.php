bb8a8fdf083b68f85d9bd175d4765340e07da4e7
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-01-11 21:06:30 +0000

    merge error

--- test/Response/WalletBalanceTest.php
+++ test/Response/WalletBalanceTest.php
@@ -5,4 +5,9 @@
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
  * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
- 
+ */
+
+namespace SakeTest\BlockchainWalletApi\Response;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Sake\BlockchainWalletApi\Response\WalletBalance;
