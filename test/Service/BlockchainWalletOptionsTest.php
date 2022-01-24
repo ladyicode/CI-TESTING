362d34e9693e42ba855215fafadc9d20a56f0cba
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-01-24 22:46:37 +0000

    Logging

--- test/Service/BlockchainWalletOptionsTest.php
+++ test/Service/BlockchainWalletOptionsTest.php
@@ -46,3 +46,130 @@ class BlockchainWalletOptionsTest extends TestCase
      */
     public function testSetHttpMethod()
     {
+        $cut = new BlockchainWalletOptions();
+        $value = \Zend\Http\Request::METHOD_POST;
+
+        $this->assertEquals(\Zend\Http\Request::METHOD_GET, $cut->getHttpMethod());
+
+        $cut->setHttpMethod($value);
+
+        $this->assertEquals($value, $cut->getHttpMethod());
+    }
+
+    /**
+     * Tests setMainPassword() and getMainPassword() should work as expected
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::setMainPassword
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::getMainPassword
+     * @group service
+     */
+    public function testSetMainPassword()
+    {
+        $cut = new BlockchainWalletOptions();
+        $value = 'secure';
+
+        $cut->setMainPassword($value);
+
+        $this->assertEquals($value, $cut->getMainPassword());
+    }
+
+    /**
+     * Tests setSecondPassword() and getSecondPassword() should work as expected
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::setSecondPassword
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::getSecondPassword
+     * @group service
+     */
+    public function testSetSecondPassword()
+    {
+        $cut = new BlockchainWalletOptions();
+        $value = 'secure second';
+
+        $cut->setSecondPassword($value);
+
+        $this->assertEquals($value, $cut->getSecondPassword());
+    }
+
+    /**
+     * Tests setUrl() and getUrl() should work as expected
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::setUrl
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::getUrl
+     * @group service
+     */
+    public function testSetUrl()
+    {
+        $cut = new BlockchainWalletOptions();
+        $value = 'https://blockchain.info/de/merchant/';
+
+        $cut->setUrl($value);
+
+        $this->assertEquals($value, $cut->getUrl());
+    }
+
+    /**
+     * Tests getResponsePluginManager() return default response plugin manager if no one provided
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::getResponsePluginManager
+     * @group service
+     */
+    public function testGetResponsePluginManagerShouldReturnDefaultResponsePluginManager()
+    {
+        $cut = new BlockchainWalletOptions();
+
+        $this->assertInstanceOf(
+            '\Sake\BlockchainWalletApi\Service\ResponsePluginManager',
+            $cut->getResponsePluginManager()
+        );
+    }
+
+    /**
+     * Tests getInputFilterPluginManager() return default input filter manager if no one provided
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::getInputFilterPluginManager
+     * @group service
+     */
+    public function testGetInputFilterPluginManagerShouldReturnDefaultFilterPluginManager()
+    {
+        $cut = new BlockchainWalletOptions();
+
+        $this->assertInstanceOf(
+            '\Sake\BlockchainWalletApi\Service\InputFilterPluginManager',
+            $cut->getInputFilterPluginManager()
+        );
+    }
+
+    /**
+     * Tests getHydrator() return class methods hydrator if no one provided
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::getHydrator
+     * @group service
+     */
+    public function testGetHydratorShouldReturnDefaultClassMethods()
+    {
+        $cut = new BlockchainWalletOptions();
+
+        $this->assertInstanceOf(
+            '\Zend\Stdlib\Hydrator\ClassMethods',
+            $cut->getHydrator()
+        );
+    }
+
+    /**
+     * Tests setHydrator()
+     *
+     * @covers \Sake\BlockchainWalletApi\Service\BlockchainWalletOptions::setHydrator
+     * @group service
+     */
+    public function testsetHydrator()
+    {
+        $cut = new BlockchainWalletOptions();
+
+        $cut->setHydrator(new \Zend\Stdlib\Hydrator\ObjectProperty());
+
+        $this->assertInstanceOf(
+            '\Zend\Stdlib\Hydrator\ObjectProperty',
+            $cut->getHydrator()
+        );
+    }
+}
