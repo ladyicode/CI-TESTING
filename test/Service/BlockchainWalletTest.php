e5cd6a6e2c8691287308c9a94f3e974d62209016
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-01-25 20:33:45 +0000

    Stage Dev iOS  #Startup Mobile Big Data (H/F) en Stage à Paris

--- test/Service/BlockchainWalletTest.php
+++ test/Service/BlockchainWalletTest.php
@@ -3,4 +3,8 @@
  * Sake
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
- * @copyright Copyright (c) 2014 San
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace SakeTest\BlockchainWalletApi\Service;
