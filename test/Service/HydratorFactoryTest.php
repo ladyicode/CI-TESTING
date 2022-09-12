79d9198eeee1a7a5082d8ef62230651626cf9891
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-09-12 15:59:22 +0000

    staging: comedi: check s->async for poll(), read() and write()

--- test/Service/HydratorFactoryTest.php
+++ test/Service/HydratorFactoryTest.php
@@ -4,4 +4,10 @@
  *
  * @link      http://github.com/sandrokeil/HydratorApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sandrokeil/HydratorApi/blob/mas
+ * @license   http://github.com/sandrokeil/HydratorApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace SakeTest\BlockchainWalletApi\Service;
+
+use Sake\BlockchainWalletApi\Service\HydratorFactory;
+use SakeTes
