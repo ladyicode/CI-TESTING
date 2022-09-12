30b73f9de2a265b787d0b8fbc262ca4b0b52f458
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-09-12 15:06:06 +0000

    fix minor bug

--- test/Service/HydratorFactoryTest.php
+++ test/Service/HydratorFactoryTest.php
@@ -2,4 +2,6 @@
 /**
  * Sake
  *
- * @link      http://github.com/sandrokeil/Hydrato
+ * @link      http://github.com/sandrokeil/HydratorApi for the canonical source repository
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/HydratorApi/blob/mas
