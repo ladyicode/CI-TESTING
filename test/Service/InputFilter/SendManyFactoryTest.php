503b218415bd32a1e87b643152be203542df3be7
ladyicode <92449416+ladyicode@users.noreply.github.com>
2022-11-25 06:45:59 +0000

    fix exception handling

--- test/Service/InputFilter/SendManyFactoryTest.php
+++ test/Service/InputFilter/SendManyFactoryTest.php
@@ -2,4 +2,9 @@
 /**
  * Sake
  *
- * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repos
+ * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+name
