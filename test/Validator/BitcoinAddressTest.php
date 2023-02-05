df40c8d4968a13da7a2da55c354447a4bb3cd3e1
ladyicode <92449416+ladyicode@users.noreply.github.com>
2023-02-05 11:28:13 +0000

    update REAMDE

--- test/Validator/BitcoinAddressTest.php
+++ test/Validator/BitcoinAddressTest.php
@@ -7,4 +7,11 @@
  * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
  */
 
-namespace SakeTest\Blockch
+namespace SakeTest\BlockchainWalletApi\Validator;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Sake\BlockchainWalletApi\Validator\BitcoinAddress;
+use Zend\Stdlib\ArrayUtils;
+
+/**
+ * Class 
