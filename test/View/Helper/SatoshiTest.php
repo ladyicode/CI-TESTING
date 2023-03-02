33edfe8a45453e085c0d80c74436a675b4f7ac21
ladyicode <92449416+ladyicode@users.noreply.github.com>
2023-03-02 18:56:16 +0000

    Fix Dead Store (Dead assignement/Dead increment) Warning found by Clang

--- test/View/Helper/SatoshiTest.php
+++ test/View/Helper/SatoshiTest.php
@@ -4,4 +4,9 @@
  *
  * @link      http://github.com/sandrokeil/BlockchainWalletApi for the canonical source repository
  * @copyright Copyright (c) 2014 Sandro Keil
- * @license   http://github.com/sa
+ * @license   http://github.com/sandrokeil/BlockchainWalletApi/blob/master/LICENSE.txt New BSD License
+ */
+
+namespace SakeTest\BlockchainWalletApi\View\Helper;
+
+use PHPUnit_Framework_
